7c19c84cf095c831b8ccd85782650b461b086673
sathya5278 <sathyamohan012@gmail.com>
2020-06-21 01:04:02 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
+  console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
 
 var app = express();
 
-var dishRouter=require('./routes/dishRouter');
-var leaderRouter=require('./routes/leaderRouter');
-var promoRouter=require('./routes/promoRouter');
+var dishRouter = require('./routes/dishRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
 
-app.use('/dishes',dishRouter);
-app.use('/leaders',leaderRouter);
-app.use('/promotions',promoRouter);
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -37,18 +37,43 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+  var auth = new Buffer.from(authHeader.split(" ")[1], 'base64').toString().split(":");
+  var username = auth[0];
+  var password = auth[1];
+  if (username === "admin" && password === "password") {
+    next();
+  }
+  else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+}
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
