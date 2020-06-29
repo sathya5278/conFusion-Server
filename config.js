a4287f51e73f82fa1e0d0e0cdfa45fa4b008885c
sathya5278 <sathyamohan012@gmail.com>
2020-06-29 20:54:34 +0530

    Facebook Password

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67891-23456-78912',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client App ID',
+        clientSecret: 'Your Client App Secret'
+    }
 }
